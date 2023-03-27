06c7b0a6db238018d372adadab65fb086e5a21e6
Joe Davis <Pup13T@aol.com>
2023-03-27 11:03:27 +0700

    a

--- main.py
+++ main.py
@@ -15,9 +15,42 @@ class Teachers:
         """
         Return person name,age,subject
         """
-        return f"Hi, my name is {self.name} and I am {self.age}. I teach {self.subject}."
+        return f"Hi, my name is {self.name} and I am {self.age}. I teach {self.subject}"
+
+class Hightenure(Teachers):
+    """
+    tenure of teacher
+    """
+    def __init__(self, name, age=30, subject="English", tenure="higher tenure"):
+        self.tenure = tenure
+        super().__init__(name, age, subject)
+
+    def introduce(self):
+        introduce= super().introduce()
+        introduce= f"{introduce} and I have {self.tenure} teaching"
+        return introduce
+class Salaryrate(Teachers):
+    """
+    Certain pay
+    """
+    def __init__(self, name, age, subject, salary):
+        self.salary = salary
+        super().__init__(name, age, subject)
+
+    def introduce(self):
+        introduce= super().introduce()
+        introduce= f"{introduce} and my salary is {self.salary}"
+        return introduce
+
 if __name__== "__main__":
     john = Teachers("John Doe")
     print(john.introduce())
     jane = Teachers("Jane Doe", 28, subject= "Math")
     print(jane.introduce())
+
+    patrick = Hightenure("Patrick Star", age=30, subject="English")
+    print(patrick.introduce())
+
+    sarah = Salaryrate("Sarah Scott", age=33, subject="Science", salary=70000)
+    print(sarah.introduce())
+    
--- test_main.py
+++ test_main.py
@@ -2,9 +2,33 @@
 Testing program, Teachers higher tenure with salary
 """
 
-from main import Teachers
+from main import Teachers, Hightenure, Salaryrate
 
-def Test_Teachers():
+def test_teachers():
+    """
+    Test Teachers class
+    """
     test_teachers1 = Teachers("John Doe", 25, "English")
-    introduce = test_teacher.introduce
+    introduce = test_teachers1.introduce()
     assert "John Doe" in introduce
+    test_teachers2 = Teachers("Jane Doe", 28, "Math")
+    introduce = test_teachers2.introduce()
+    assert "Jane Doe" in introduce
+
+def test_hightenure():
+    """
+    Test high tenure
+    """
+    testhightenure1 = Hightenure("Patrick Star", 30, "English", "higher tenure")
+    introduce = testhightenure1.introduce()
+    assert "Patrick Star" in introduce
+    assert "higher tenure" in introduce
+
+def test_salaryrate():
+    """
+    Test salary
+    """
+    testsalaryrate1 = Salaryrate("Sarah Scott", 33, "Science", 70000)
+    introduce = testsalaryrate1.introduce()
+    assert "Sarah Scott" in introduce
+    assert "70000" in introduce
