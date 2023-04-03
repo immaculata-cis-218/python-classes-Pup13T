b7c963c1a4ff5c700aa5c4f29d47d164478fe026
Joe Davis <Pup13T@aol.com>
2023-04-03 07:32:19 -0400

    a

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ Create a Class for week 8 through week 10
 By Joe Davis
 """
 
+
 class Teachers:
     """
     Teachers class for name, age, subject:math,english,ect.
     """
+
     def __init__(self, name, age=25, subject="English"):
         self.name = name
         self.age = age
@@ -15,13 +17,13 @@ class Teachers:
     def __repr__(self):
         """
         Returns a complete representation of the object
-         """
+        """
         return f"Teacher(name='{self.name}', age={self.age}, subject='{self.subject}')"
 
     def __str__(self):
         """
         Returns a human readable string
-         """
+        """
         return f"{self.name} {self.subject} teacher."
 
     def __eq__(self, other):
@@ -50,10 +52,12 @@ class Teachers:
         """
         return f"Hi, my name is {self.name} and I am {self.age}. I teach {self.subject}"
 
+
 class Hightenure(Teachers):
     """
     tenure of teacher
     """
+
     def __init__(self, name, age=30, subject="English", tenure="higher tenure"):
         self.tenure = tenure
         super().__init__(name, age, subject)
@@ -80,29 +84,32 @@ class Hightenure(Teachers):
         return self.tenure >= other.tenure
 
     def introduce(self):
-        introduce= super().introduce()
-        introduce= f"{introduce} and I have {self.tenure} teaching"
+        introduce = super().introduce()
+        introduce = f"{introduce} and I have {self.tenure} teaching"
         return introduce
+
 
 class Salaryrate(Teachers):
     """
     Certain pay
     """
+
     def __init__(self, name, age, subject, salary):
         self.salary = salary
         super().__init__(name, age, subject)
 
     def introduce(self):
-        introduce= super().introduce()
-        introduce= f"{introduce} and my salary is {self.salary}"
+        introduce = super().introduce()
+        introduce = f"{introduce} and my salary is {self.salary}"
         return introduce
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     john = Teachers("John Doe")
     print(repr(john))
     print(str(john))
 
-    jane = Teachers("Jane Doe", age=28, subject= "Math")
+    jane = Teachers("Jane Doe", age=28, subject="Math")
     print(jane.introduce())
     print(str(jane))
 
